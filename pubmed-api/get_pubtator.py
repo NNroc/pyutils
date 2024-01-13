2847c019acad8e1047a8cc4d4e4087a9e16546fc
NNroc <1581333251@qq.com>
2024-01-13 10:50:22 +0800

    update

--- pubmed-api/get_pubtator.py
+++ pubmed-api/get_pubtator.py
@@ -6,36 +6,38 @@ def cmp(x):
 
 
 Entrez.email = "123@example.com"
+term = "ebola[Title/Abstract]"
 
 search_results1 = Entrez.read(
-    Entrez.esearch(db="pubmed", term="ebola virus", reldate=50000, retmax=99999, datetype="pdat", usehistory="y",
-                   mindate="1976/01/01", maxdate="2023/01/01"))
+    Entrez.esearch(db="pubmed", term=term, reldate=50000, retmax=99999, datetype="pdat", usehistory="y",
+                   mindate="2014/01/01", maxdate="2024/01/01"))
 count = int(search_results1["Count"])
 print("Found %i results" % count)
 
-search_results2 = Entrez.read(
-    Entrez.esearch(db="pubmed", term="ebola hemorrhagic fever", reldate=50000, retmax=99999, datetype="pdat",
-                   usehistory="y", mindate="1976/01/01", maxdate="2023/01/01"))
-count = int(search_results2["Count"])
-print("Found %i results" % count)
-
-search_results = search_results1["IdList"] + search_results2["IdList"]
+search_results = search_results1["IdList"]
 search_results = list(set(search_results))
 search_results = sorted(search_results, key=cmp)
 print(len(search_results))
 
+handle = Entrez.efetch(db="pubmed", id=search_results, rettype="medline", retmode="text")
+records = list(Medline.parse(handle))
 num = 0
 
 with open('./ebola.pubtator', 'w', encoding='utf-8') as file:
-    for i in search_results:
+    for record in records:
         try:
-            print(num, i)
-            handle = Entrez.efetch(db="pubmed", id=i, rettype="medline", retmode="text")
-            record = list(Medline.parse(handle))[0]
+            i = record['PMID']
+            print(record['PMID'])
             title = record['TI'] if 'TI' in record else 'nan'  # 标题
             abtxt = record['AB'] if 'AB' in record else 'nan'  # 摘要
             ebola_num = title.lower().count('ebola') + abtxt.lower().count('ebola')
+            ebola_all = title.lower().count('ebola') + abtxt.lower().count('ebola') \
+                        + abtxt.count('EVD') + abtxt.count('EBOV') + abtxt.count('EHF')
             if ebola_num < 2:
+                print(title)
+                print(abtxt)
+                continue
+            if ebola_all < 5:
                 print(title)
                 print(abtxt)
                 continue
@@ -47,5 +49,3 @@ with open('./ebola.pubtator', 'w', encoding='utf-8') as file:
             num += 1
         except ConnectionResetError:
             print(i)
-
-print()
--- pubmed-api/pmid.py
+++ pubmed-api/pmid.py
@@ -1,43 +1,47 @@
 # 检测重复度
 
-ebola_pmid = set()
+ebola_pmid = list()
 with open('./ebola.pubtator', 'r', encoding='utf-8') as file:
     lines = file.readlines()
     for line in lines:
         if len(line.split('|')) >= 3 and line.split('|')[1] == 't':
-            ebola_pmid.add(line.split('|')[0])
+            ebola_pmid.append(line.split('|')[0])
 
-cdr_pmid = set()
-with open('./cdr/CDR_DevelopmentSet.PubTator.txt', 'r', encoding='utf-8') as file:
-    lines = file.readlines()
-    for line in lines:
-        if len(line.split('|')) >= 3 and line.split('|')[1] == 't':
-            cdr_pmid.add(line.split('|')[0])
-with open('./cdr/CDR_TestSet.PubTator.txt', 'r', encoding='utf-8') as file:
-    lines = file.readlines()
-    for line in lines:
-        if len(line.split('|')) >= 3 and line.split('|')[1] == 't':
-            cdr_pmid.add(line.split('|')[0])
-with open('./cdr/CDR_TrainingSet.PubTator.txt', 'r', encoding='utf-8') as file:
-    lines = file.readlines()
-    for line in lines:
-        if len(line.split('|')) >= 3 and line.split('|')[1] == 't':
-            cdr_pmid.add(line.split('|')[0])
+with open('./pmid.txt', 'w', encoding='utf-8') as file:
+    for pmid in ebola_pmid:
+        file.write(pmid + '\n')
 
-gda_pmid = set()
-with open('./gda/testing.pubtator', 'r', encoding='utf-8') as file:
-    lines = file.readlines()
-    for line in lines:
-        if len(line.split('|')) >= 3 and line.split('|')[1] == 't':
-            gda_pmid.add(line.split('|')[0])
-with open('./gda/training.pubtator', 'r', encoding='utf-8') as file:
-    lines = file.readlines()
-    for line in lines:
-        if len(line.split('|')) >= 3 and line.split('|')[1] == 't':
-            gda_pmid.add(line.split('|')[0])
-
-set1 = ebola_pmid.union(cdr_pmid)
-set2 = ebola_pmid.union(gda_pmid)
-print(len(ebola_pmid) + len(cdr_pmid) - len(set1))
-print(len(ebola_pmid) + len(gda_pmid) - len(set2))
-print()
+# cdr_pmid = set()
+# with open('./cdr/CDR_DevelopmentSet.PubTator.txt', 'r', encoding='utf-8') as file:
+#     lines = file.readlines()
+#     for line in lines:
+#         if len(line.split('|')) >= 3 and line.split('|')[1] == 't':
+#             cdr_pmid.add(line.split('|')[0])
+# with open('./cdr/CDR_TestSet.PubTator.txt', 'r', encoding='utf-8') as file:
+#     lines = file.readlines()
+#     for line in lines:
+#         if len(line.split('|')) >= 3 and line.split('|')[1] == 't':
+#             cdr_pmid.add(line.split('|')[0])
+# with open('./cdr/CDR_TrainingSet.PubTator.txt', 'r', encoding='utf-8') as file:
+#     lines = file.readlines()
+#     for line in lines:
+#         if len(line.split('|')) >= 3 and line.split('|')[1] == 't':
+#             cdr_pmid.add(line.split('|')[0])
+#
+# gda_pmid = set()
+# with open('./gda/testing.pubtator', 'r', encoding='utf-8') as file:
+#     lines = file.readlines()
+#     for line in lines:
+#         if len(line.split('|')) >= 3 and line.split('|')[1] == 't':
+#             gda_pmid.add(line.split('|')[0])
+# with open('./gda/training.pubtator', 'r', encoding='utf-8') as file:
+#     lines = file.readlines()
+#     for line in lines:
+#         if len(line.split('|')) >= 3 and line.split('|')[1] == 't':
+#             gda_pmid.add(line.split('|')[0])
+#
+# set1 = ebola_pmid.union(cdr_pmid)
+# set2 = ebola_pmid.union(gda_pmid)
+# print(len(ebola_pmid) + len(cdr_pmid) - len(set1))
+# print(len(ebola_pmid) + len(gda_pmid) - len(set2))
+# print()
