6b30c1b71b4696506ede486eaaa32c94ee0cecf6
NNroc <1581333251@qq.com>
2024-03-30 15:03:01 +0800

    update

--- test/test2.py
+++ test/test2.py
@@ -1,6 +1,33 @@
-import re
+# Definition for a binary tree node.
 
-text = "In LPA2-reconstituted MEF cells lacking LPA1    '3 the levels of gamma-H2AX decreased rapidly, whereas in Vector MEF were high and remained sustained."
+import queue
 
-print(re.sub(r'\s+', ' ', text))
-print(text)
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root == None:
+            return 0
+        ans = 0
+        q = queue.Queue()
+        q.put(root)
+        while q.qsize() > 0:
+            node = q.get()
+            if node == None:
+                continue
+            else:
+                q.put(node.left)
+                q.put(node.right)
+            ans += 1
+        return ans
+
+
+right = TreeNode(2)
+root = TreeNode(1, None, right)
+Solution.maxDepth(None, root)
