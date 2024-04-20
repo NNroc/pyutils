52ab02aa2925aadc807d4135192846b9639cddca
NNroc <1581333251@qq.com>
2024-04-20 16:35:35 +0800

    update

--- test/test2.py
+++ test/test2.py
@@ -1,33 +1,4 @@
-# Definition for a binary tree node.
+import pandas as pd
 
-import queue
-
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
-class Solution:
-    def maxDepth(self, root: TreeNode) -> int:
-        if root == None:
-            return 0
-        ans = 0
-        q = queue.Queue()
-        q.put(root)
-        while q.qsize() > 0:
-            node = q.get()
-            if node == None:
-                continue
-            else:
-                q.put(node.left)
-                q.put(node.right)
-            ans += 1
-        return ans
-
-
-right = TreeNode(2)
-root = TreeNode(1, None, right)
-Solution.maxDepth(None, root)
+filename = '../同性恋数据/中国城市同性恋数据.xlsx'
+df = pd.read_excel(filename, index_col=0)
